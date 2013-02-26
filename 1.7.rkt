470640f6277245ca91fe967a991ef2577aa02542
Julia Zhydanova <jz@anahoret.com>
2013-02-26 16:22:45 +0200

    Improve task 1.7.

--- 1.7.rkt
+++ 1.7.rkt
@@ -2,22 +2,10 @@
 (define (square x)
   (* x x))
 (define (good-enough guess prev-guess x)
-  (display "[")
-  (display guess)
-  (display " ")
-  (display prev-guess)
-  (display " ")
-  (display (abs (- guess prev-guess )))
-  (display "]")
-  ; Не похоже, что в таком виде процедура работает лучше. Возможно точнее, т.к. на очень мелких числах результат сильно отличается в десятых и сотых.
-  (< (abs (- guess prev-guess )) 0.001))
+  ;Видно, что при такой погрешности результаты становятся более точными для мелких чисел. На большие числа это не влияет.
+  (< (/ (abs (- guess prev-guess )) guess) 0.001))
 
 (define (good-enough? guess prev x)
-  (display "[")
-  (display guess)
-  (display " ")
-  (display (abs (- (square guess) x)))
-  (display "]")
   (< (abs (- (square guess) x)) 0.001))
 
 (define (average x y)
@@ -27,22 +15,29 @@
   (average guess (/ x guess)))
 
 (define (sqrt-iter guess prev-guess x)
-  (if (good-enough? guess prev-guess x)
+  (if (good-enough guess prev-guess x)
       guess
       (sqrt-iter (improve guess x) guess x)))
 
 (define (sqrt x)
   (sqrt-iter 1.0 0 x))
 
-(sqrt 0.1)
-;;good-enough? 
-; sqrt 0.0000001
-;0.03125106561775382
-; sqrt 100000000000
-; 316227.7660168379
+(sqrt 0.0000001)
+(sqrt 100000000000)
 
+;;good-enough? 
+;sqrt(0.0000001) = 0.03125106561775382
+;square(0.03125106561775382) = 0.000976629102245155
+
+;sqrt(100000000000) = 316227.7660168379
+;square(316227.7660168379) = 100000000001.20638
+
+;-------------------------------------------
 ;;good-enough 
-; sqrt 0.0000001
-;0.0010104595507340792
-; sqrt 100000000000
-; 316227.7660168379
+;sqrt(0.0000001) = 0.00031622776664863746
+;square(0.00031622776664863746) = 1.000000003995851e-07
+
+;sqrt(100000000000) = 316227.7660168379
+;square(316227.7660168379) = 100000000001.20638
+
+
