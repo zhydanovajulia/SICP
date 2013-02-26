c6427c2ae8d047861b271c460824bb04ac04e7d1
Julia Zhydanova <jz@anahoret.com>
2013-02-26 16:46:14 +0200

    Improve 1.8 again.

--- 1.8.rkt
+++ 1.8.rkt
@@ -11,9 +11,16 @@
 (define (improve guess x)
   (third x guess))
 
-(define (cube-root guess prev x)
+(define (cube-iter guess prev x)
   (if (good-enough? guess prev x)
       guess
-      (cube-root (improve guess x) guess x)))
+      (cube-iter (improve guess x) guess x)))
+(define (cube-root x)
+  (cube-iter 1.0 0 x))
 
-(cube-root 1.0 0 64) 
+(cube-root 64) ; 4.000017449510739 ; 64.00083758016928
+(cube-root 0.000000000009) ; 0.08779149544457145 ;0.0006766394902792291
+(cube-root 100000000000000) ;46415.88833612779 ;1e+14
+(* 4.000017449510739 4.000017449510739 4.000017449510739)
+(* 0.08779149544457145 0.08779149544457145 0.08779149544457145)
+(* 46415.88833612779 46415.88833612779 46415.88833612779)
