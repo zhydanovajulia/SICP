ec44fde60e45d55bb0867041fbc3011729dc849f
Julia Zhydanova <jz@anahoret.com>
2013-03-06 15:49:42 +0200

    Again update 1.7.

--- 1.7.rkt
+++ 1.7.rkt
@@ -2,12 +2,12 @@
 (define (square x)
   (* x x))
 (define (good-enough guess prev-guess x)
-  (display (/ (abs (- guess prev-guess )) guess))
   ;Видно, что при такой погрешности результаты становятся более точными для мелких чисел. На большие числа это не влияет.
-  (< (/ (abs (- guess prev-guess )) guess) 0.001))
+  (or (< (/ (abs (- guess prev-guess )) guess) 0.001)
+      (< (abs (- guess prev-guess )) 0.001)))
 
-(define (good-enough? guess prev x)
-  (< (abs (- (square guess) x)) 0.001))
+;(define (good-enough? guess prev x)
+;  (< (abs (- (square guess) x)) 0.001))
 
 (define (average x y)
   (/ (+ x y) 2))
@@ -21,16 +21,16 @@
       (sqrt-iter (improve guess x) guess x)))
 
 (define (sqrt x)
-  (cond ((or (= x 0) (= x 1)) x)
-        (else (sqrt-iter 1.0 0 x))))
+  (sqrt-iter 1.0 0 x))
 
-(sqrt 1.0) 
-;(sqrt 0.0000001)
-;(sqrt 100000000000)
+
+(sqrt 0.0) 
+(sqrt 0.0000001)
+(sqrt 100000000000)
 
 ;;good-enough? 
-;sqrt(0.0000001) = 0.03125106561775382
-;square(0.03125106561775382) = 0.000976629102245155
+;sqrt(0.0000001) = 0.0010104595507340792
+(square 0.0010104595507340792) = 1.0210285036697172e-06
 
 ;sqrt(100000000000) = 316227.7660168379
 ;square(316227.7660168379) = 100000000001.20638
