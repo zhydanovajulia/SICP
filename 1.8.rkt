8a2989fe6e52ff040d9d66d15bb58b45fd907d79
Julia Zhydanova <jz@anahoret.com>
2013-03-15 14:42:53 +0200

    Fix cubic.

--- 1.8.rkt
+++ 1.8.rkt
@@ -6,7 +6,7 @@
   (/ (+ (/ x (square y)) (* 2 y)) 3))
 
 (define (good-enough? guess prev x)
-  (< (/ (abs (- guess prev)) guess) 0.001))
+  (< (/ (abs (- guess prev)) (abs guess)) 0.001))
 
 (define (improve guess x)
   (third x guess))
@@ -18,7 +18,7 @@
 (define (cube-root x)
   (cube-iter 1.0 0 x))
 
-(cube-root 64) ; 4.000000000076121 ; 64.00000000365382
+(cube-root -64) ; 4.000000000076121 ; 64.00000000365382
 (cube-root 0.000000000009) ; 0.00020800843914385545 ;9.000007377800638e-12
 (cube-root 100000000000000) ;46415.88834018749 ;1e+14
 (* 4.000000000076121 4.000000000076121 4.000000000076121)
